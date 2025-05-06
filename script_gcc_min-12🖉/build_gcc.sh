8a9eb7d4bcfdcce2af248efbf096e4b8e7cef6b4
Thomas Walker Lynch <eknp9n@reasoningtechnology.com>
2025-05-06 07:43:47 -0700

    baseline set, gcc-12.1.0 compiled

--- script_gcc_min-12🖉/build_gcc.sh
+++ script_gcc_min-12🖉/build_gcc.sh
@@ -10,17 +10,19 @@ echo "🔧 Starting GCC build..."
 mkdir -p "$GCC_BUILD"
 pushd "$GCC_BUILD"
 
-"$GCC_SRC/configure" \
+echo "gcc: $(command -v gcc)"
+echo "toolchain: $TOOLCHAIN"
 
+"$GCC_SRC/configure" \
   --with-pkgversion="RT_gcc standalone by Reasoning Technology" \
   --with-bugurl="https://github.com/Thomas-Walker-Lynch/RT_gcc/issues" \
   --with-documentation-root-url="https://gcc.gnu.org/onlinedocs/" \
   --with-changes-root-url="https://github.com/Thomas-Walker-Lynch/RT_gcc/releases/" \
-
   --host="$HOST" \
   --prefix="$TOOLCHAIN" \
   --enable-languages=c,c++ \
   --enable-threads=posix \
+  --disable-multilib 
 
 $MAKE -j"$MAKE_JOBS"
 $MAKE install
